3a8aaa6e070bffe53749a2599cdbc4f25c98670a
Tobias Årud <me@tobiasarud.se>
2016-08-29 14:32:31 +0200

    @jesperorb Gäller uppgiften i AJAX med jQuery

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 // jQuery coded by: Tobias Årud
 $(document).ready(function(){
+    // GETs Flickr API pictures from a partiicular user. This one uses getJSON rather than get.
     $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?id=28590554@N05&lang=en-us&format=json&jsoncallback=?", function(data){
         var photos = "";
         $.each(data.items, function(i, item){
             photos += '<div class="column-1-6"><img class="responsive" src="' + item.media.m + '"></div>';
         });
         $(".gallery-content").append(photos);
+    });
+    
+    // Showing understanding of how to POST to API using jQuery.
+    var inputNamn = $("#namn");
+    var inputBetyg = $("#betyg");
+    $("#addcontent").submit(function(e){
+        e.preventDefault();
+        $.post("https://bakverksapi.herokuapp.com/bakverk", {namn: inputNamn.val(), betyg: inputBetyg.val()})
     });
 });
